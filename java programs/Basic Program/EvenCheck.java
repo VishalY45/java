82193ec02e46606ca1f10c86f73c8a23393b4532
vishal yadav <vy897268@gmail.com>
2024-03-01 12:08:51 +0530

    Check EVen Odd Program

---
+++ java programs/Basic Program/EvenCheck.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class EvenCheck
+{
+	public static void main(String s[])
+	{
+		int no=10;
+		if(no%2==0)
+		{
+			System.out.println("No Is Even");
+		}
+		else
+		{
+			System.out.println("No is Odd");
+		}
+	}
+}
+			
